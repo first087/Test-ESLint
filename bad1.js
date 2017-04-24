b25a8253b4c99b2b1a448f7d0e526e7d06aad24e
Artit Kiuwilai <first087@gmail.com>
2017-04-25 04:55:24 +0700

    Code fixed

--- bad1.js
+++ bad1.js
@@ -5,17 +5,17 @@
 //creates libaries stuffz
 function elementliBRaryContent(a,b,c,d) {
 
-var bnam = a;var baut = b;
-var bdat = c;
-var bcont = d;
-var self = { findword: function(x) { return bcont.indexOf(x)>=0;},
- findremword: function(y) { return bcont.indexOf(y)>=0?bcont.replace(y + ' ',''):bcont;},
-  findrepword: function(z,w) { return bcont.indexOf(z)>=0?bcont.replace(z,w):bcont;},
-getbnam: function() {return bnam;},
-getbaut: function() {return baut;},
-getbdat: function() {return bdat;},
-getbcont: function() {return bcont;}
-};
+    var bnam = a;var baut = b
+    var bdat = c
+    var bcont = d
+    var self = { findword: function(x) { return bcont.indexOf(x)>=0},
+        findremword: function(y) { return bcont.indexOf(y)>=0?bcont.replace(y + ' ',''):bcont},
+        findrepword: function(z,w) { return bcont.indexOf(z)>=0?bcont.replace(z,w):bcont},
+        getbnam: function() {return bnam},
+        getbaut: function() {return baut},
+        getbdat: function() {return bdat},
+        getbcont: function() {return bcont}
+    }
 
-return self;
-};
+    return self
+}
--- bad2.js
+++ bad2.js
@@ -1,25 +1,25 @@
 // From https://j11y.io/javascript/javascript-bad-practices/
 
-var Color = 'red';
-var SomeNumber = 234;
-var AnArray = [1,2,3];
-var Foo = { bar: 123 };
+var Color = 'red'
+var SomeNumber = 234
+var AnArray = [1,2,3]
+var Foo = { bar: 123 }
 // None of them should start with a capital letter!
 
-foo = 3; // NO!
-var foo = 3; // YES
+foo = 3 // NO!
+var foo = 3 // YES
 
-var someVar1 = 'a';
-var someVar2 = 'b';
-var someVar3 = 'c';
+var someVar1 = 'a'
+var someVar2 = 'b'
+var someVar3 = 'c'
 
 // Much easier:
-var someVar1 = 'a',
-    someVar2 = 'b',
-    someVar3 = 'c';
+someVar1 = 'a'
+someVar2 = 'b'
+someVar3 = 'c'
 
-1 == "1"; // true
-false == " nt "; // true
-[[],[]] == true; // true
+1 == '1' // true
+false == ' nt '; // true
+[[],[]] == true // true
 
 // ... Confused?
