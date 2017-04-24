5c86a6a8d1c8c32af9fc15ddd3224c541f2ec7a5
Artit Kiuwilai <first087@gmail.com>
2017-04-25 06:20:13 +0700

    Code fixed

--- bad1.js
+++ bad1.js
@@ -1,21 +1,20 @@
 // From https://gist.github.com/glauberramos/3087548
-//this function represents a book from an online library
-//it has 3 methods to search the book, remove a specific word and change it
-//it has 4 attributes, name, author, date and content
-//creates libaries stuffz
-function elementliBRaryContent(a,b,c,d) {
+// this function represents a book from an online library
+// it has 3 methods to search the book, remove a specific word and change it
+// it has 4 attributes, name, author, date and content
+// creates libaries stuffz
+function elementliBRaryContent(a, b, c, d) {
+  const bnam = a; const baut = b;
+  const bdat = c;
+  const bcont = d;
+  const self = { findword(x) { return bcont.indexOf(x) >= 0; },
+    findremword(y) { return bcont.indexOf(y) >= 0 ? bcont.replace(`${y} `, '') : bcont; },
+    findrepword(z, w) { return bcont.indexOf(z) >= 0 ? bcont.replace(z, w) : bcont; },
+    getbnam() { return bnam; },
+    getbaut() { return baut; },
+    getbdat() { return bdat; },
+    getbcont() { return bcont; },
+  };
 
-var bnam = a;var baut = b;
-var bdat = c;
-var bcont = d;
-var self = { findword: function(x) { return bcont.indexOf(x)>=0;},
- findremword: function(y) { return bcont.indexOf(y)>=0?bcont.replace(y + ' ',''):bcont;},
-  findrepword: function(z,w) { return bcont.indexOf(z)>=0?bcont.replace(z,w):bcont;},
-getbnam: function() {return bnam;},
-getbaut: function() {return baut;},
-getbdat: function() {return bdat;},
-getbcont: function() {return bcont;}
-};
-
-return self;
-};
+  return self;
+}
